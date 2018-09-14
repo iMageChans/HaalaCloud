2d00e6d2c0f7381d331b6dc25c18f474a71ab303
Chans <imagechans@gmail.com>
2018-09-14 19:08:28 +0800

    update GUI for windows

--- src/Delegate/progressbardelegate.cpp
+++ src/Delegate/progressbardelegate.cpp
@@ -1,6 +1,51 @@
-#include "progressbardelegate.h"
+﻿#include "progressbardelegate.h"
 
-ProgressBarDelegate::ProgressBarDelegate()
+#include <QProgressBar>
+
+ProgressBarDelegate::ProgressBarDelegate(QWidget *parent)
+    : QStyledItemDelegate(parent)
 {
 
+}
+
+void ProgressBarDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+    QStyleOptionViewItem viewOption(option);
+    initStyleOption(&viewOption, index);
+    if (option.state.testFlag(QStyle::State_HasFocus))
+        viewOption.state = viewOption.state ^ QStyle::State_HasFocus;
+
+    QStyledItemDelegate::paint(painter, viewOption, index);
+
+    if (index.column() == FILE_DOWNLOAD_PROGRESS_COLUMN)
+    {
+        int nProgress = index.model()->data(index, Qt::UserRole).toInt();
+        int nLeft = 8;
+        int nTop = 8;
+        int nWidth = option.rect.width() - 2 * nLeft;
+        int nHeight = option.rect.height() - 2 * nTop;
+
+        // 设置进度条的风格
+        QStyleOptionProgressBar progressBarOption;
+        progressBarOption.initFrom(option.widget);
+        // 设置进度条显示的区域
+        progressBarOption.rect = QRect(option.rect.left() + nLeft, option.rect.top() + nTop,  nWidth, nHeight);
+        // 设置最小值
+        progressBarOption.minimum = 0;
+        // 设置最大值
+        progressBarOption.maximum = 100;
+        // 设置对齐方式
+        progressBarOption.textAlignment = Qt::AlignCenter;
+        // 设置进度
+        progressBarOption.progress = nProgress;
+        // 设置文本（百分比）
+        progressBarOption.text = QString("%1%").arg(nProgress);
+        // 设置文本可见
+        progressBarOption.textVisible = true;
+
+        QProgressBar progressBar;
+
+        //绘制进度条
+        QApplication::style()->drawControl(QStyle::CE_ProgressBar, &progressBarOption, painter, &progressBar);
+    }
 }
--- src/Delegate/progressbardelegate.h
+++ src/Delegate/progressbardelegate.h
@@ -1,12 +1,30 @@
-#ifndef PROGRESSBARDELEGATE_H
+﻿#ifndef PROGRESSBARDELEGATE_H
 #define PROGRESSBARDELEGATE_H
 
 #include <QObject>
+#include <QStyledItemDelegate>
+#include <QApplication>
 
-class ProgressBarDelegate
+#define FILE_DOWNLOAD_FILE_NAME_COLUMN           0
+#define FILE_DOWNLOAD_SIZE_COLUMN                1
+#define FILE_DOWNLOAD_STATUS_COLUMN              2
+#define FILE_DOWNLOAD_PROGRESS_COLUMN            3
+
+// 下载记录
+struct FileDownloadRecord
 {
+    QString strFileName;         //文件名称
+    qint64 nSize;                //大小
+    int nStatus;                 //状态
+    int nProgress;               //进度
+};
+
+class ProgressBarDelegate : public QStyledItemDelegate
+{
+    Q_OBJECT
 public:
-    ProgressBarDelegate();
+    ProgressBarDelegate(QWidget *parent = nullptr);
+    void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
 };
 
 #endif // PROGRESSBARDELEGATE_H
